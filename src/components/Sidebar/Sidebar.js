90d96a746e6e0616bc1bd64429c367a17bf282dd
Emerson Silva <emerson_jdss@hotmail.com>
2022-08-07 08:21:57 -0300

    change name of company

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -37,16 +37,16 @@ function Sidebar({ color, image, routes }) {
       />
       <div className="sidebar-wrapper">
         <div className="logo d-flex align-items-center justify-content-start">
-          <a
+          {/* <a
             href="https://www.creative-tim.com?ref=lbd-sidebar"
             className="simple-text logo-mini mx-1"
           >
             <div className="logo-img">
-              <img src={require("assets/img/reactlogo.png")} alt="..." />
+              <img src={require("assets/img/pepsico.png")} alt="..." />
             </div>
-          </a>
-          <a className="simple-text" href="http://www.creative-tim.com">
-            Creative Tim
+          </a> */}
+          <a className="simple-text" href="https://www.pepsico.com.br/">
+            Pepsico
           </a>
         </div>
         <Nav>
